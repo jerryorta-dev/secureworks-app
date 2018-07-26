8671e79b6fb9a628815851aed87e8cbad6e616a5
Jerry Orta <jerryorta.dev@gmail.com>
2018-07-25 19:16:48 -0500

    oops

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
   production: true,
-  firebase: {
-    apiKey: 'AIzaSyBAHsiCcDUo0rLjwoEe8Ac1pRZHsx6Wokg',
-    authDomain: 'works-app-303e3.firebaseapp.com',
-    databaseURL: 'https://works-app-303e3.firebaseio.com',
-    projectId: 'works-app-303e3',
-    storageBucket: 'works-app-303e3.appspot.com',
-    messagingSenderId: '550831023163'
-  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyBAHsiCcDUo0rLjwoEe8Ac1pRZHsx6Wokg',
-    authDomain: 'works-app-303e3.firebaseapp.com',
-    databaseURL: 'https://works-app-303e3.firebaseio.com',
-    projectId: 'works-app-303e3',
-    storageBucket: 'works-app-303e3.appspot.com',
-    messagingSenderId: '550831023163'
-  }
 };
 
 /*
